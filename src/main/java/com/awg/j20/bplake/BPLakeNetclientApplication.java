401fb0933255aa85578c91b5856e93b674dd80f8
zobarov <zobarov@gmail.com>
2020-02-12 20:45:49 +0000

    Initialize project

---
+++ src/main/java/com/awg/j20/bplake/BPLakeNetclientApplication.java
@@ -0,0 +1,13 @@
+package com.awg.j20.bplake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BPLakeNetclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BPLakeNetclientApplication.class, args);
+	}
+
+}
